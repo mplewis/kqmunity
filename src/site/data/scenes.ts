6b2eed04a375da90f543ba1dea37a5390047a517
Matt Lewis <matt@mplewis.com>
2024-12-09 22:04:33 -0700

    wip: scenes list

--- src/site/data/scenes.ts
+++ src/site/data/scenes.ts
@@ -1,5 +1,12 @@
+import DenverSkyline from "../images/denver-skyline.webp";
+
 export const scenes = {
   denver: {
-    discord: { guild_id: "1276250547787268127", timezone: "America/Denver" },
+    name: "Denver",
+    splash: {
+      image: DenverSkyline,
+      alt: "Denver skyline against the Rocky Mountains",
+    },
+    discord: { guildID: "1276250547787268127", timezone: "America/Denver" },
   },
 };
