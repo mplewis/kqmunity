6d15d45aee09afbb30588fcadb82c4b8ed915ce8
Matt Lewis <matt@mplewis.com>
2024-12-09 22:15:02 -0700

    wip: add links to other cities; render tiles

--- src/site/data/scenes.ts
+++ src/site/data/scenes.ts
@@ -9,4 +9,54 @@ export const scenes = {
     },
     discord: { guildID: "1276250547787268127", timezone: "America/Denver" },
   },
+  slc: {
+    name: "Salt Lake City",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://kqslc.com",
+  },
+  portland: {
+    name: "Portland",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://killerqueenpdx.buzz",
+  },
+  jacksonville: {
+    name: "Jacksonville",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://kqjax.com",
+  },
+  southFlorida: {
+    name: "South Florida",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://www.facebook.com/groups/KillerQueenSouthFlorida",
+  },
+  chicago: {
+    name: "Chicago",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://www.facebook.com/groups/mercurysquadchicago",
+  },
+  indianapolis: {
+    name: "Indianapolis",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://www.facebook.com/groups/348252818671598",
+  },
+  baltimore: {
+    name: "Baltimore",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://www.facebook.com/groups/baltimorekillerqueen",
+  },
+  sanFrancisco: {
+    name: "San Francisco",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://www.facebook.com/groups/KillerQueenSF",
+  },
+  seattle: {
+    name: "Seattle",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "https://www.facebook.com/groups/KQSeattle",
+  },
+  minneapolis: {
+    name: "Minneapolis",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    externalSite: "http://killerqueenmpls.com",
+  },
 };
