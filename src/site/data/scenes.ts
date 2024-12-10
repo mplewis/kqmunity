a08c05dd2421674a3bae1402751deb8f94a43616
Matt Lewis <matt@mplewis.com>
2024-12-09 22:22:29 -0700

    feat: support for external sites

--- src/site/data/scenes.ts
+++ src/site/data/scenes.ts
@@ -1,62 +1,98 @@
 import DenverSkyline from "../images/denver-skyline.webp";
 
-export const scenes = {
+type Scene = {
+  name: string;
+  splash: { image: ImageMetadata; alt: string };
+  site: { kind: "internal" } | { kind: "external"; href: string };
+  discord?: { guildID: string; timezone: string };
+};
+
+export const scenes: Record<string, Scene> = {
+  baltimore: {
+    name: "Baltimore",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/baltimorekillerqueen",
+    },
+  },
+
+  chicago: {
+    name: "Chicago",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/mercurysquadchicago",
+    },
+  },
+
   denver: {
     name: "Denver",
     splash: {
       image: DenverSkyline,
       alt: "Denver skyline against the Rocky Mountains",
     },
+    site: { kind: "internal" },
     discord: { guildID: "1276250547787268127", timezone: "America/Denver" },
   },
-  slc: {
-    name: "Salt Lake City",
-    splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://kqslc.com",
-  },
-  portland: {
-    name: "Portland",
-    splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://killerqueenpdx.buzz",
-  },
-  jacksonville: {
-    name: "Jacksonville",
-    splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://kqjax.com",
-  },
-  southFlorida: {
-    name: "South Florida",
-    splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://www.facebook.com/groups/KillerQueenSouthFlorida",
-  },
-  chicago: {
-    name: "Chicago",
-    splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://www.facebook.com/groups/mercurysquadchicago",
-  },
+
   indianapolis: {
     name: "Indianapolis",
     splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://www.facebook.com/groups/348252818671598",
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/348252818671598",
+    },
   },
-  baltimore: {
-    name: "Baltimore",
+
+  jacksonville: {
+    name: "Jacksonville",
     splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://www.facebook.com/groups/baltimorekillerqueen",
+    site: { kind: "external", href: "https://kqjax.com" },
   },
-  sanFrancisco: {
-    name: "San Francisco",
-    splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://www.facebook.com/groups/KillerQueenSF",
-  },
-  seattle: {
-    name: "Seattle",
-    splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "https://www.facebook.com/groups/KQSeattle",
-  },
+
   minneapolis: {
     name: "Minneapolis",
     splash: { image: DenverSkyline, alt: "TODO" },
-    externalSite: "http://killerqueenmpls.com",
+    site: { kind: "external", href: "http://killerqueenmpls.com" },
+  },
+
+  portland: {
+    name: "Portland",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    site: { kind: "external", href: "https://killerqueenpdx.buzz" },
+  },
+
+  sanFrancisco: {
+    name: "San Francisco",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/KillerQueenSF",
+    },
+  },
+
+  seattle: {
+    name: "Seattle",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/KQSeattle",
+    },
+  },
+
+  slc: {
+    name: "Salt Lake City",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    site: { kind: "external", href: "https://kqslc.com" },
+  },
+
+  southFlorida: {
+    name: "South Florida",
+    splash: { image: DenverSkyline, alt: "TODO" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/KillerQueenSouthFlorida",
+    },
   },
 };
