b45bc9aa626e981dfc544feb881c77b77824e7e6
Matt Lewis <matt@mplewis.com>
2024-12-09 23:56:01 -0700

    Build page for local scenes (#2)
    # Content
    - **Add page for known scenes with external website links**
    - Add About Us page with contact info
    
    # Styles
    - Add sane defaults for `<a>`, `<p>`, `<h1...6>` via `@apply`
    - Improve styles of existing elements
    - Use `<Button>` on main page
    - Cap content width in Denver page
    - Use consistent font sizes for headers
    
    # Other
    - Add support for `<MainLayout title="...">`
    - Update crontab to run hourly site rebuild less frequently

--- src/site/data/guilds.ts
+++
@@ -1,3 +0,0 @@
-export const guilds = {
-  KQ_DEN: { id: "1276250547787268127", timezone: "America/Denver" },
-};
---
+++ src/site/data/scenes.ts
@@ -0,0 +1,114 @@
+import Baltimore from "../images/scenes/baltimore.jpg";
+import Chicago from "../images/scenes/chicago.jpg";
+import Denver from "../images/scenes/denver.jpg";
+import Indianapolis from "../images/scenes/indianapolis.jpg";
+import Jacksonville from "../images/scenes/jacksonville.jpg";
+import Minneapolis from "../images/scenes/minneapolis.jpg";
+import Portland from "../images/scenes/portland.jpg";
+import SF from "../images/scenes/sf.jpg";
+import Seattle from "../images/scenes/seattle.jpg";
+import SLC from "../images/scenes/slc.jpg";
+import SouthFlorida from "../images/scenes/south-florida.jpg";
+
+type Scene = {
+  name: string;
+  splash: { image: ImageMetadata; alt: string };
+  site: { kind: "internal" } | { kind: "external"; href: string };
+  discord?: { guildID: string; timezone: string };
+};
+
+export const scenes = {
+  baltimore: {
+    name: "Baltimore",
+    splash: { image: Baltimore, alt: "Baltimore skyline over the water" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/baltimorekillerqueen",
+    },
+  },
+
+  chicago: {
+    name: "Chicago",
+    splash: { image: Chicago, alt: "Chicago skyline over the lake" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/mercurysquadchicago",
+    },
+  },
+
+  denver: {
+    name: "Denver",
+    splash: {
+      image: Denver,
+      alt: "Denver skyline against the Rocky Mountains",
+    },
+    site: { kind: "internal" },
+    discord: { guildID: "1276250547787268127", timezone: "America/Denver" },
+  },
+
+  indianapolis: {
+    name: "Indianapolis",
+    splash: {
+      image: Indianapolis,
+      alt: "Fireworks over the Indianapolis skyline",
+    },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/348252818671598",
+    },
+  },
+
+  jacksonville: {
+    name: "Jacksonville",
+    splash: { image: Jacksonville, alt: "Jacksonville skyline and bridge" },
+    site: { kind: "external", href: "https://kqjax.com" },
+  },
+
+  minneapolis: {
+    name: "Minneapolis",
+    splash: {
+      image: Minneapolis,
+      alt: "Minneapolis skyline and Stone Arch Bridge",
+    },
+    site: { kind: "external", href: "http://killerqueenmpls.com" },
+  },
+
+  portland: {
+    name: "Portland",
+    splash: { image: Portland, alt: "Portland skyline and bridge" },
+    site: { kind: "external", href: "https://killerqueenpdx.buzz" },
+  },
+
+  sanFrancisco: {
+    name: "San Francisco",
+    splash: { image: SF, alt: "Golden Gate Bridge" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/KillerQueenSF",
+    },
+  },
+
+  seattle: {
+    name: "Seattle",
+    splash: { image: Seattle, alt: "Seattle skyline and Mt. Helena" },
+    site: {
+      kind: "external",
+      href: "https://www.facebook.com/groups/KQSeattle",
+    },
+  },
+
+  slc: {
+    name: "Salt Lake City",
+    splash: { image: SLC, alt: "Salt Lake City skyline against the mountains" },
+    site: { kind: "external", href: "https://kqslc.com" },
+  },
+
+  "south-florida": {
+    name: "South Florida",
+    splash: { image: SouthFlorida, alt: "Ft. Lauderdale on the ocean" },
+    site: {
+      kind: "external",
+      href: "https://kqsfl.com",
+    },
+  },
+} as const satisfies Record<string, Scene>;
